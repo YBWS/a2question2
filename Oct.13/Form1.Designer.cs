8ed095cf0d0e7c44220d7b8d535b51705ca5580d
YBWS <b.yang@sjasd.ca>
2016-10-19 10:46:13 -0500

    bowen
    bowen

---
+++ Oct.13/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Oct._13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.Location = new System.Drawing.Point(257, 121);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(209, 320);
+            this.lblDisplay.TabIndex = 0;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(107, 252);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(107, 179);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 2;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(107, 118);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(107, 330);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 20);
+            this.txt4.TabIndex = 4;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(107, 421);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 20);
+            this.txt5.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(107, 495);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(119, 50);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Calculate Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(30, 118);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(32, 13);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "item1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(33, 185);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(32, 13);
+            this.lbl2.TabIndex = 8;
+            this.lbl2.Text = "item2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(30, 259);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(32, 13);
+            this.lbl3.TabIndex = 9;
+            this.lbl3.Text = "item3";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(36, 336);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(32, 13);
+            this.lbl4.TabIndex = 10;
+            this.lbl4.Text = "item4";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(33, 427);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(32, 13);
+            this.lbl5.TabIndex = 11;
+            this.lbl5.Text = "item5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 681);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.lblDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+    }
+}
+
---
+++ Oct.13/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oct._13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /**
+
+ *
+
+ * Name: Bowen
+
+ * Teacher: Mr.Hardman
+
+ * Assignment #2, Program #2
+
+ * Date Last Modified: Oct.14
+
+ *
+
+ */
+           
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double item1 = Convert.ToDouble(txt1.Text);
+            double item2 = Convert.ToDouble(txt2.Text);
+            double item3 = Convert.ToDouble(txt3.Text);
+            double item4 = Convert.ToDouble(txt4.Text);
+            double item5 = Convert.ToDouble(txt5.Text);
+
+            double average = (item1 + item2 + item3 + item4 + item5) / 5.0;
+            lblDisplay.Text = "Average"+ average;
+            
+            //Display all caculate to the user
+        
+           
+
+            
+
+        }
+    }
+}
